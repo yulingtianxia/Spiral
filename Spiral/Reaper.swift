158c52776d6b1cfe68d98b9ec6d4ac0edd3ed969
yulingtianxia <yulingtianxia@gmail.com>
2014-10-23 18:14:30 +0800

    修复zooming，zoom out写反，调整reaper发光颜色

--- Spiral/Reaper.swift
+++ Spiral/Reaper.swift
@@ -13,7 +13,7 @@ class Reaper: Shape {
         self.init(name:"Reaper",imageName:"reaper")
         self.physicsBody!.categoryBitMask = reaperCategory
         self.moveSpeed = 250
-        light.lightColor = SKColor.blueColor()
+        light.lightColor = SKColor(red: 78.0/255, green: 146.0/255, blue: 223.0/255, alpha: 1)
         light.categoryBitMask = reaperLightCategory
         light.enabled = true
     }
