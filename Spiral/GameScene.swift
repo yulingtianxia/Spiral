d316f231a9c3be0ff2db06a4d3b9313d76d2cf07
yulingtianxia <yulingtianxia@gmail.com>
2015-06-13 20:06:37 +0800

    sb不小心删了个文件,现在补上

---
+++ Spiral/GameScene.swift
@@ -0,0 +1,62 @@
+//
+//  GameScene.swift
+//  Spiral
+//
+//  Created by 杨萧玉 on 14-7-12.
+//  Copyright (c) 2014年 杨萧玉. All rights reserved.
+//
+
+import SpriteKit
+
+@objc protocol GameControlProtocol{
+    func pause()
+    func tap()
+    func createReaper()
+    func allShapesJumpIn()
+    
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate, GameControlProtocol{
+    let soundManager = SoundManager()
+    var player:Player
+    
+    override init(size:CGSize){
+        player = Player()
+        soundManager.playBackGround()
+        super.init(size: size)
+        addChild(soundManager)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func pause() {
+        
+    }
+    
+    func tap() {
+        
+    }
+    
+    func createReaper() {
+        
+    }
+    
+    func allShapesJumpIn() {
+        
+    }
+    
+    //MARK: SKPhysicsContactDelegate
+    
+    func didBeginContact(contact:SKPhysicsContact){
+        //A->B
+        let visitorA = ContactVisitor.contactVisitorWithBody(contact.bodyA, forContact: contact)
+        let visitableBodyB = VisitablePhysicsBody(body: contact.bodyB)
+        visitableBodyB.acceptVisitor(visitorA)
+        //B->A
+        let visitorB = ContactVisitor.contactVisitorWithBody(contact.bodyB, forContact: contact)
+        let visitableBodyA = VisitablePhysicsBody(body: contact.bodyA)
+        visitableBodyA.acceptVisitor(visitorB)
+    }
+}
